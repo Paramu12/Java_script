68d0412ccc20626829e2d6e9e9a381dddfc8b1b3
paramukutty <pparameswaria@gmail.com>
2025-11-08 14:09:26 +0530

    netural

---
+++ conditional/task1.js
@@ -0,0 +1,13 @@
+//check the given number is negative or positive or netural
+let a=-34
+if(a>0){
+  console.log("postive")
+}
+else if(a<0){
+  console.log("negative")
+}
+else{
+  console.log("netural")
+}
+//expected output
+//negative
