cf79aa2624d6dc37a5414e59f1ad0b9649124799
paramukutty <pparameswaria@gmail.com>
2025-11-08 15:58:29 +0530

    Trafficlight

---
+++ conditional/task6.js
@@ -0,0 +1,13 @@
+//Traffic light red yellow green message
+let a="stop"
+if(a==="stop"){
+    console.log("red")
+}
+else if(a==="wait"){
+    console.log("yellow")
+}
+else{
+    console.log("green")
+}
+//expected output
+//red
