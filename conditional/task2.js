f379d8f6967890eaa3dd544540234f5bbd082c0c
paramukutty <pparameswaria@gmail.com>
2025-11-08 15:39:56 +0530

    largestnumber

---
+++ conditional/task2.js
@@ -0,0 +1,15 @@
+//Find the largest of three number
+let a=10
+let b=98
+let c=59
+if((a>b)&&(a>c)){
+    console.log("a is greater")
+}
+else if(b>c){
+    console.log("b is greater")
+}
+else{
+    console.log("c is greater")
+}
+//expected output
+//b is greater
