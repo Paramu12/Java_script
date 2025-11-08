fc28a1464a5bb11523c5d4126352e4d4fb3e16de
paramukutty <pparameswaria@gmail.com>
2025-11-08 15:51:42 +0530

    divisible3and5

---
+++ conditional/task4.js
@@ -0,0 +1,10 @@
+//check if the number is divisible by 3 and 5
+let x=15
+ if((x%3===0)&&(x%5===0)){
+    console.log("the number divisible by 3 and 5")
+ }
+ else{
+    console.log("not divisible by 3 and 5")
+ }
+ //expected output
+ //the number divisible by 3 and 5
