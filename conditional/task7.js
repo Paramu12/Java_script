81d0809036ad81794a2b9f8da1ff30707f222ad3
paramukutty <pparameswaria@gmail.com>
2025-11-08 16:02:24 +0530

    Trainticket

---
+++ conditional/task7.js
@@ -0,0 +1,16 @@
+//Train ticket book:
+//aged person above(60)=>lower birth
+//middle person/ladies(30-50)=>middle birth
+//teen age (below 30)=>upper birth
+let a=45
+if(a>60){
+  console.log("Lower Birth")
+}
+else if((a>30)&&(a<=50)){
+  console.log("Middle Birth")
+}
+else{
+  console.log("Upper Birth")
+}
+//expected output
+//Middle Birth
