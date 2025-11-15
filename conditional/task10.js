6da75c407dbc1c0a1a4ca385d10cef68483b4405
paramukutty <pparameswaria@gmail.com>
2025-11-15 20:58:14 +0530

    fibanocci

---
+++ conditional/task10.js
@@ -0,0 +1,10 @@
+//let a=0
+//let b=1
+//let add,i
+//while(i<=10){
+  //    add=a+b
+    //  a=b
+  //    b=add
+    //  console.log(b)
+     // i++
+//}
---
+++ conditional/task11.js
@@ -0,0 +1,6 @@
+ let num=1
+ for(let i=1;i<=5;i++){
+     num=num*i
+    
+}
+ console.log(num)
